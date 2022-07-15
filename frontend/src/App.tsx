cccef45fb34a0466f2692b83fb62bcce0370906b
Baltazar Barbosa <baltazarbarbosaimoveis@gmail.com>
2022-07-15 16:37:11 -0300

    Primeiro componente

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,12 @@
+import NotificationButton from "./components/NotificationBotton"
 
 function App() {
-return(
-  <h1>Ola mundo</h1>
-)
+  return (
+    <>
+      <h1>Ola mundo</h1>
+      <NotificationButton />
+    </>
+  )
 }
 
 export default App
